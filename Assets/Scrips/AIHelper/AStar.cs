3cea2e5cb7ca297af183340b0e549659690a2543
Corentin Royer <corentin.35035@gmail.com>
2022-01-21 16:48:26 +0100

    First commit

---
+++ Assets/Scrips/AIHelper/AStar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStar
+{
+    
+    public static void ComputeShortestPath(float[,] traversability, Vector2Int startPosition, Vector2Int goalPosition) {
+
+        
+
+    }
+}
